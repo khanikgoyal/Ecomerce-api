ff7c7b3cc95eca37ab06ec3aa2b255a69af9904f
khanikgoyal <khanikgoyal87@gmail.com>
2025-08-29 16:07:51 +0530

    first commit

---
+++ server/config/connectDB.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+if(!process.env.MONGODB_URI){
+    throw new Error("please provide the mongodb uri inside .env file");
+}
+
+async function connectDB(){
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("DB connected")
+    } catch (error) {
+        console.log("MongoDB connection failed", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import connectDB from './config/connectDB.js';
+
+const app = express();
+app.use(cors({
+    credentials : true,
+    origin : "process.env.FRONTEND_URL"
+    }
+));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan());
+app.use(helmet({
+    crossOriginEmbedderPolicy : false,
+}));
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res)=>{
+    res.json({message : "Server is running"}); 
+});
+connectDB().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`);
+    })
+});
